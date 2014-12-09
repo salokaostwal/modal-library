80aeecd92145ecf65f33dcb2307fcc4dba2f04ac
vaibhav <vaibhav.vgc@gmail.com>
2014-12-09 17:00:06 +0530

    dynamic modal text-content

--- scripts/modals.js
+++ scripts/modals.js
@@ -3,7 +3,8 @@ var modal_maker = (function() {
 
     var modalstring = document.createElement('div'),
         modal_container = document.querySelector('.modal-area'),
-        modal_overlay = document.createElement('div');
+        modal_overlay = document.createElement('div'),
+        text_content = document.createElement('div');
 
     modal_overlay.classList.add('modal-overlay');
     modalstring.innerHTML = "<button class='modal-close-btn'>Close</button>";
@@ -20,6 +21,8 @@ var modal_maker = (function() {
         modal_container.appendChild(modalstring);
         modalstring.classList.add('init');
         appendToBody(modal_overlay);
+        text_content.textContent = this.dataset.modalText;
+        modalstring.appendChild(text_content);
     };
     var closeModal = function(evt) {
         if (evt.target.classList.contains('modal-close-btn')) {
