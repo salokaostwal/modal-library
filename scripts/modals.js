2617da2364dc920a9f24a379cff7f3e8da30f598
vaibhav <vaibhav.vgc@gmail.com>
2014-12-09 15:31:32 +0530

    object name changes

--- scripts/modals.js
+++ scripts/modals.js
@@ -2,22 +2,22 @@ var modal_maker = (function() {
   'use strict';
 
   var modalstring = document.createElement('div'),
-    $modal_container = document.querySelector('.modal-area'),
-    $body = document.querySelector('body');
+    modal_container = document.querySelector('.modal-area'),
+    body = document.querySelector('body');
   modalstring.innerHTML = "<button class='modal-close-btn'>close</button>";
   modalstring.classList.add('modal-wrap');
 
   var toggleOverlay = function() {
-    $body.classList.toggle('overlay');
+    body.classList.toggle('overlay');
   };
   var invokeModal = function() {
-    $modal_container.appendChild(modalstring);
+    modal_container.appendChild(modalstring);
     modalstring.classList.add('init');
     toggleOverlay();
   };
   var closeModal = function(evt) {
     if(evt.target.classList.contains('modal-close-btn')) {
-      $modal_container.removeChild(modalstring);
+      modal_container.removeChild(modalstring);
       toggleOverlay();
     }
   };
