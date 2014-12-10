54fd75e8752ba4e04b4fb5802a62d5626d923e54
divi-k <poonamkamble4587@gmail.com>
2014-12-10 16:12:12 +0530

    modal display-- by using display block and none

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,30 @@
-var overlayMask = document.createElement('div');
-	overlayMask.className = 'overlay-mask';
-	document.body.appendChild(overlayMask);
+document.addEventListener('DOMContentLoaded', function(){
+  var overlayMask = document.createElement('div');
+  	overlayMask.className = 'overlay-mask';
+  var overlayContainer = document.createElement('div');
+    overlayContainer.className = 'overlay-container';
+    overlayContainer.innerHTML = '<div class="close"></div><h3>This is the modal content</h3>';
+
+  document.body.appendChild(overlayMask);
+  document.body.appendChild(overlayContainer);
+
+  document.querySelector('.overlay-trigger').onclick = function(){	
+  	overlayMask.style.display = 'block';
+    overlayContainer.style.display = 'block';
+  }
+
+  overlayMask.addEventListener("click",function(){	
+  	overlayMask.style.display = 'none';
+    overlayContainer.style.display = 'none';
+  });
 
 
-document.querySelector('.overlay-trigger').onclick = function(){	
-	overlayMask.style.display = 'block';
-}
+  var btnclose = document.querySelector(".close");
+    btnclose.addEventListener("click",function(){  
+      overlayMask.style.display = 'none';
+      overlayContainer.style.display = 'none';
+    });
+}, false);
 
-overlayMask.addEventListener("click",function(){	
-	overlayMask.style.display = 'none';
-});
+
+
